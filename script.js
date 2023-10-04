d864c603854b649af5ea6c25558a0e6d781c6693
Dennis Swiggett <dennisswiggett11@gmail.com>
2023-10-04 14:36:35 -0400

    Added function that allows computer to choose rock, paper, scissors

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function getComputerChoice() {
-    
+    let choice = Math.floor(Math.random() * 9) + 1;
+    switch (choice) {
+        case 1:
+        case 2:
+        case 3:
+            return console.log('rock');
+        case 4:
+        case 5:
+        case 6:
+            return console.log('paper');
+        case 7:
+        case 8:
+        case 9:
+            return console.log('scissors');
+    }
 }
+getComputerChoice();
